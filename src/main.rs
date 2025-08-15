3bcf37e20a7d67e3e6265c03168b105a8ee8f494
Mahesh Bansod <mahesh0bansod@gmail.com>
2025-08-15 17:37:04 +0530

    create hello and goodbye functions

--- src/main.rs
+++ src/main.rs
@@ -3,5 +3,14 @@
 /// Probably the best implementation of hello world this
 /// world's ever known
 fn main() {
+    print_hello();
+    print_goodbye();
+}
+
+fn print_goodbye() {
+    println!("Goodbye, world!");
+}
+
+fn print_hello() {
     println!("Hello, world!");
 }
