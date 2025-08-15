166c94d8e3e91109d9507cb999a32d9a9fff9dab
Mahesh Bansod <mahesh0bansod@gmail.com>
2025-08-15 17:34:42 +0530

    add more comments

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,7 @@
 /// Hello world program
+///
+/// Probably the best implementation of hello world this
+/// world's ever known
 fn main() {
     println!("Hello, world!");
     println!("Goodbye, world!");
